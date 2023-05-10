2586586292eb7446908ff680806b0da545148b72
kimfame <renownkim@gmail.com>
2023-05-10 18:58:44 +0900

    Add custom JWT Authentication to load both User and Profile

--- redhorse/settings/local.py
+++ redhorse/settings/local.py
@@ -28,7 +28,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "core.authentication.CustomJWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
