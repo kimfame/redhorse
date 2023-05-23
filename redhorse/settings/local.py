7a31a4b432e4aaa7d139178d7e4d5e79c703843b
kimfame <renownkim@gmail.com>
2023-05-23 22:26:45 +0900

    Remove debug toolbar

--- redhorse/settings/local.py
+++ redhorse/settings/local.py
@@ -55,17 +55,6 @@ SIMPLE_JWT = {
 }
 
 
-# Django Debug Toolbar
-
-INSTALLED_APPS.append("debug_toolbar")
-
-MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
-
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
-
 # Logging
 
 LOGGING = {
